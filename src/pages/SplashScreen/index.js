649b8a4d270da67bc6bae3276251d420462c8de4
adiapr <da801100@gmail.com>
2023-02-22 21:35:25 +0700

    Custome Fonts

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -6,7 +6,7 @@ const SplashScreen = () => {
     return(
         <View style={{ backgroundColor: '#FFC700', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Logo /> 
-            <Text style={{ fontSize: 32, color: '#000', marginTop: 32 }}>Food Market</Text>
+            <Text style={{ fontSize: 32, color: '#000', marginTop: 32, fontFamily: 'poppins-medium' }}>Food Market</Text>
 
         </View>
     )
