428a8c973040d5f6b254cdaa759a8a179125b4eb
adiapr <da801100@gmail.com>
2023-02-26 17:33:16 +0700

    Custom Bottom navigator

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -1,3 +1,9 @@
 import IcKembali from './kembali.svg';
+import IcHomeOn from './homeOn.svg';
+import IcHomeOff from './homeOff.svg';
+import IcOrderOn from './orderOn.svg';
+import IcOrderOff from './ordrerOff.svg';
+import IcProfileOn from './profile_on.svg';
+import IcProfileOff from './profile_off.svg';
 
-export {IcKembali}; 
+export {IcKembali, IcHomeOn, IcHomeOff, IcOrderOff, IcOrderOn, IcProfileOff, IcProfileOn}; 
